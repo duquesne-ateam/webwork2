20a805f4f5b0f63935266fe4dfbf01dd16da4b70
Aeternitaas <ketmengjc@gmail.com>
2017-04-22 09:55:09 -0400

    Testing changes

--- lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor2.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor2.pm
@@ -31,12 +31,14 @@ use strict;
 use warnings;
 #use CGI qw(-nosticky );
 use WeBWorK::CGI;
-use WeBWorK::Utils qw(readFile surePathToFile path_is_subdir jitar_id_to_seq seq_to_jitar_id x);
+use WeBWorK::CGI;
 use HTML::Entities;
 use URI::Escape;
-use WeBWorK::Utils qw(has_aux_files not_blank);
+use WeBWorK::Utils qw(x surePathToFile has_aux_files not_blank readFile writeLog writeCourseLog encodeAnswers decodeAnswers is_restricted
+	ref2string makeTempDirectory path_is_subdir sortByName before after between wwRound is_jitar_problem_closed is_jitar_problem_hidden jitar_problem_adjusted_status jitar_id_to_seq seq_to_jitar_id jitar_problem_finished);
 use File::Copy;
 use File::Basename qw(dirname);
+use File::Path qw(rmtree);
 use WeBWorK::Utils::Tasks qw(fake_user fake_set renderProblems);
 use Data::Dumper;
 use Fcntl;
@@ -125,13 +127,14 @@ use Fcntl;
 # save_to_new_file
 # 
 
-use constant ACTION_FORMS => [qw(view  save save_as add_problem revert)]; 
+use constant ACTION_FORMS => [qw(view save save_as add_problem revert upload)]; 
 use constant ACTION_FORM_TITLES => {   # for use with tabber it is important that the titles have no spaces
 view        => x("View"),
 add_problem => x("Append"),
 save        => x("Update"),
 save_as     => x("NewVersion"),
 revert      => x("Revert"),
+upload	    => x("UploadVideo")
 };
 #[qw(view save save_as revert add_problem add_header make_local_copy)];
 
@@ -144,6 +147,7 @@ use constant FORM_PERMS => {
 		save_as => "modify_student_data",
 #		rename  => "modify_student_data",
 		revert => "modify_student_data",
+		upload => "modify_student_data",
 };
 
 our $BLANKPROBLEM = 'blankProblem.pg';
@@ -476,7 +480,7 @@ sub body {
 	my $inputFilePath   = $self->{inputFilePath};   # path to the file for input, (might be a .tmp file)
 	my $setName         = $self->{setID} ;
 	my $problemNumber   = $self->{problemID} ;
-    $setName            = defined($setName) ? $setName : '';  # we need this instead of using the || construction 
+	$setName            = defined($setName) ? $setName : '';  # we need this instead of using the || construction 
                                                               # to keep set 0 from being set to the 
                                                               # empty string.
 	my $fullSetName = defined( $self->{fullSetID} ) ? $self->{fullSetID} : $setName;
@@ -1933,14 +1937,14 @@ sub save_as_handler {
 	my $relativeOutputFilePath = $self->getRelativeSourceFilePath($outputFilePath);
 	
 	my $viewURL = $self->systemLink($problemPage, 
-								 params=>{
-									 sourceFilePath     => $relativeOutputFilePath, #The path relative to the templates directory is required.
-									 problemSeed        => $problemSeed,
-									 edit_level         => $edit_level,
-									 file_type          => $new_file_type,
-									 status_message     => uri_escape($self->{status_message})
+						 params=>{
+							 sourceFilePath     => $relativeOutputFilePath, #The path relative to the templates directory is required.
+							 problemSeed        => $problemSeed,
+							 edit_level         => $edit_level,
+							 file_type          => $new_file_type,
+							 status_message     => uri_escape($self->{status_message})
 
-								 }
+							 }
 	);
 	
 	$self->reply_with_redirect($viewURL);
@@ -1970,6 +1974,63 @@ sub revert_handler {
 	$self->{r_problemContents} = \$problemContents;
 	$self->addgoodmessage("Reverting to original file '".$self->shortPath($editFilePath)."'");
 	# no redirect is needed
+}
+
+sub upload_form {
+	my ($self, $onChange, %actionParams) = @_;
+	my $r = $self->r;
+
+	return join(" ",
+		CGI::start_form(
+			-method=>"POST",
+			-enctype=>'multipart/form-data',
+			-name=>"form",
+		),
+		CGI::input({type => "file", name => "file", id=> "file", size => 40, maxlength => 80}),
+	);
+}
+
+sub upload_handler {
+	my $self = shift;
+	my $r = $self->r;
+	my $ce = $self->r->ce;
+	my $urlpath = $r->urlpath;
+	my $setID = $r->urlpath->arg("setID");
+	my $problemNumber = $r->urlpath->arg("problemID");
+	my $newFolderName = "$setID"."_Problem_$problemNumber"."_Student_Uploads";
+
+	my $fileIDhash = $self->r->param('file');
+	unless ($fileIDhash) {
+	    $self->addbadmessage("You have not chosen a file to upload.");
+	    return;
+	}
+	my ($id, $hash) = split(/\s+/,$fileIDhash);
+	my $dir = $ce->{courseDirs}->{templates}.'/'."set$setID".'/'.$newFolderName;
+	my $upload = WeBWorK::Upload->retrieve($id,$hash,dir=>$self->{ce}{webworkDirs}{uploadCache});
+	my $name = checkName($upload->filename);
+	my $file = "$dir/$name";
+	$upload->disposeTo($file);
+
+	if (-e $file) {
+	    $self->addgoodmessage("File '$name' uploaded successfully");
+	    if ($name =~ m/\.(tar|tar\.gz|tgz)$/ && $self->getFlag('unpack')) {
+	      if ($self->unpack($name) && $self->getFlag('autodelete')) {
+	        if (unlink($file)) {$self->addgoodmessage("Archive '$name' delete")}
+		  else {$self->addbadmessage("Can't delete archive '$name': $!")}
+	      }
+	   }
+	}
+
+	return $r->maketext("Your file has been uploaded successfully.");
+}
+
+sub checkName {
+	my $file = shift;
+	$file =~ s!.*[/\\]!!;               # remove directory
+	$file =~ s/[^-_.a-zA-Z0-9 ]/_/g;    # no illegal characters
+	$file =~ s/^\./_/;                  # no initial dot
+	$file = "newfile.txt" unless $file; # no blank names
+	return $file;
 }
 
 sub output_JS{
